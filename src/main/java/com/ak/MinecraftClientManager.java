469fae980ae23ae5ca1e176d97b1fd5c9edb05bb
Ak <ads9964@gmail.com>
2024-07-30 01:55:47 +0200

    Refact MinecraftClientManager

--- src/main/java/com/ak/MinecraftClientManager.java
+++ src/main/java/com/ak/MinecraftClientManager.java
@@ -5,31 +5,36 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MinecraftClientManager {
     private static final Logger log = LoggerFactory.getLogger(MinecraftClientManager.class);
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public static void startMainThread(String host, int port, int iterations, int sleepTime, List<ProxyInfo> proxies) {
-        Thread mainThread = new Thread(() -> {
-            for (int i = 0; i < iterations; i++) {
-                try {
-                    Thread.sleep(sleepTime);
-                } catch (InterruptedException e) {
-                    log.warn("Main thread interrupted.", e);
-                    Thread.currentThread().interrupt();
-                    return;
-                }
+        if (proxies == null || proxies.isEmpty()) {
+            log.warn("No proxies available.");
+            return;
+        }
 
-                Thread thread = new Thread(() -> {
-                    try {
-                        MinecraftClient.login(UserGenerator.generateRandomUser(), proxies.get((int) (Math.random() * proxies.size())), host, port);
-                    } catch (Exception e) {
-                        log.error("Error during login.", e);
-                    }
-                });
-                thread.start();
+        Runnable task = () -> {
+            for (int i = 0; i < iterations; i++) {
+                scheduler.schedule(() -> startClient(host, port, proxies), (long) i * sleepTime, TimeUnit.MILLISECONDS);
             }
-        });
+        };
+
+        Thread mainThread = new Thread(task);
         mainThread.start();
+    }
+
+    private static void startClient(String host, int port, List<ProxyInfo> proxies) {
+        ProxyInfo proxy = proxies.get((int) (Math.random() * proxies.size()));
+        try {
+            MinecraftClient.login(UserGenerator.generateRandomUser(), proxy, host, port);
+        } catch (Exception e) {
+            log.error("Error during login.", e);
+        }
     }
 }
