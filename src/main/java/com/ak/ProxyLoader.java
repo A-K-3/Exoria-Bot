85b292132140297ac52ebef652245b0fb3bd2c7d
Ak <ads9964@gmail.com>
2024-07-29 20:42:40 +0200

    Add ProxyLoader

---
+++ src/main/java/com/ak/ProxyLoader.java
@@ -0,0 +1,46 @@
+package com.ak;
+
+import org.geysermc.mcprotocollib.network.ProxyInfo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProxyLoader {
+
+    public static List<ProxyInfo> loadProxies(File file) {
+        List<ProxyInfo> proxies = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String protocol = "";
+                if (line.startsWith("http://")) {
+                    protocol = "HTTP";
+                    line = line.substring(7);
+                } else if (line.startsWith("socks5://")) {
+                    protocol = "SOCKS5";
+                    line = line.substring(9);
+                } else if (line.startsWith("socks4://")) {
+                    protocol = "SOCKS4";
+                    line = line.substring(9);
+                }
+
+                String[] parts = line.split(":");
+                String ip = parts[0];
+                int port = Integer.parseInt(parts[1]);
+
+                ProxyInfo.Type type = ProxyInfo.Type.valueOf(protocol);
+                ProxyInfo proxy = new ProxyInfo(type, ip, port);
+                proxies.add(proxy);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return proxies;
+    }
+}
