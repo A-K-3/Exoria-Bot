86f037d0095d02abbd0f95178ce4fc1670f20eab
Ak <ads9964@gmail.com>
2024-07-30 01:55:53 +0200

    Refact ProxyLoader

--- src/main/java/com/ak/ProxyLoader.java
+++ src/main/java/com/ak/ProxyLoader.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProxyLoader {
 
@@ -21,57 +22,59 @@ public class ProxyLoader {
         if (Files.notExists(proxiesPath)) {
             try {
                 Files.createFile(proxiesPath);
-                log.info("Proxies file not found. Created empty proxies file at " + filePath);
+                log.info("Proxies file not found. Created empty proxies file at {}", filePath);
             } catch (IOException e) {
                 log.error("Error creating proxies file.", e);
             }
             return new ArrayList<>();
         }
-        return ProxyLoader.loadProxies(proxiesPath);
+        return loadProxiesFromFile(proxiesPath);
     }
 
-    private static List<ProxyInfo> loadProxies(Path proxiesPath) {
-        List<ProxyInfo> proxies = new ArrayList<>();
-
+    private static List<ProxyInfo> loadProxiesFromFile(Path proxiesPath) {
         try (BufferedReader br = Files.newBufferedReader(proxiesPath)) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String protocol = "";
-                if (line.startsWith("http://")) {
-                    protocol = "HTTP";
-                    line = line.substring(7);
-                } else if (line.startsWith("socks5://")) {
-                    protocol = "SOCKS5";
-                    line = line.substring(9);
-                } else if (line.startsWith("socks4://")) {
-                    protocol = "SOCKS4";
-                    line = line.substring(9);
-                } else {
-                    log.warn("Unknown protocol in line: " + line);
-                    continue;
-                }
-
-                String[] parts = line.split(":");
-                if (parts.length != 2) {
-                    log.warn("Invalid proxy format in line: " + line);
-                    continue;
-                }
-
-                try {
-                    String ip = parts[0];
-                    int port = Integer.parseInt(parts[1]);
-
-                    ProxyInfo.Type type = ProxyInfo.Type.valueOf(protocol);
-                    ProxyInfo proxy = new ProxyInfo(type, ip, port);
-                    proxies.add(proxy);
-                } catch (NumberFormatException e) {
-                    log.warn("Invalid port number in line: " + line);
-                }
-            }
+            return br.lines()
+                    .map(ProxyLoader::parseProxyLine)
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             log.error("Error reading proxies file: ", e);
+            return new ArrayList<>();
+        }
+    }
+
+    private static ProxyInfo parseProxyLine(String line) {
+        String protocol = determineProtocol(line);
+        if (protocol.isEmpty()) {
+            log.warn("Unknown protocol in line: {}", line);
+            return null;
         }
 
-        return proxies;
+        String[] parts = line.split(":");
+        if (parts.length != 2) {
+            log.warn("Invalid proxy format in line: {}", line);
+            return null;
+        }
+
+        try {
+            String ip = parts[0];
+            int port = Integer.parseInt(parts[1]);
+            ProxyInfo.Type type = ProxyInfo.Type.valueOf(protocol);
+            return new ProxyInfo(type, ip, port);
+        } catch (NumberFormatException e) {
+            log.warn("Invalid port number in line: {}", line);
+            return null;
+        }
+    }
+
+    private static String determineProtocol(String line) {
+        if (line.startsWith("http://")) {
+            return "HTTP";
+        } else if (line.startsWith("socks5://")) {
+            return "SOCKS5";
+        } else if (line.startsWith("socks4://")) {
+            return "SOCKS4";
+        } else {
+            return "";
+        }
     }
 }
